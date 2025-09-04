9ef17292bd848fd07500d0fd6b4645262a29ef3b
GIT-WANG123 <2846686282@qq.com>
2025-09-04 13:48:02 +0800

    bugclear

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,8 @@ module.exports = {
     proxy: {
       '/api': {
         target: "http://39.97.33.178",
-        changeOrigin: true
+        changeOrigin: true,
+        allowedHosts:'all'
       }  
     }
   }
